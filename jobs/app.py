438caecfee39de1e75e7bbb67c32cb9c8d5a13bd
Ubuntu <ubuntu@ip-172-31-21-141.ec2.internal>
2021-12-03 15:10:46 +0000

    Completed module

--- jobs/app.py
+++ jobs/app.py
@@ -6,14 +6,14 @@ PATH = 'db/jobs.sqlite'
 app = Flask(__name__)
 
 def open_connection():
-    connection = getattr(g,_connection,None)
+    connection = getattr(g, '_connection',None)
     if connection == None:
         connection = g._connection = sqlite3.connect(PATH)
-        connetion.row_factory = sqlite3.Row
+        connection.row_factory = sqlite3.Row
         return  connection
 
 def execute_sql(sql,values=(),commit=False,single=False):
-    open_connection()
+    connection = open_connection()
     cursor = connection.execute(sql,values)
     if commit == True:
         results =connection.commit()
@@ -23,8 +23,9 @@ def execute_sql(sql,values=(),commit=False,single=False):
     cursor.close()
     return results
 
+@app.teardown_appcontext
 def close_connection(exception):
-    connection= getattr(g,_connection, None)
+    connection= getattr(g,'_connection', None)
     if connection is not None:
         connection.close()
 
