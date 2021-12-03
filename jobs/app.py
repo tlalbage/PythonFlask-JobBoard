ec6f4b8526483f610a083f15cbd5989963065457
Ubuntu <ubuntu@ip-172-31-21-141.ec2.internal>
2021-12-03 13:51:55 +0000

    Completed module

---
+++ jobs/app.py
@@ -0,0 +1,36 @@
+import sqlite3
+from flask import Flask, render_template, g
+
+PATH = 'db/jobs.sqlite'
+
+app = Flask(__name__)
+
+def open_connection():
+    connection = get attr(g, _connection, None)
+    if connection == None:
+        connection = g._connection = sqlite3.connect(PATH)
+        connetion.row_factory = sqlite3.Row
+        return  connection
+
+def execute_sql(sql,values=(),commit=False,single=False):
+    open_connection()
+    cursor = connection.execute(sql,values)
+    if commit == True:
+        results =connection.commit()
+    else:
+        results =cursor.fetchone() if single else fetchall()
+
+    cursor.close()
+    return results
+
+def close_connection(exception):
+    connection= getattr(g,'_connection', None)
+    if connection is not None:
+        connection.close()
+
+
+
+@app.route('/')
+@app.route('/jobs')
+def jobs():
+    return render_template('index.html')
